5418b5434d09d86247ba3b6f969b008c21e18e10
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-18 07:06:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,22 @@ class Song
     @@all.sort_by! { |song| song.name }
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    @@all << new_song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
